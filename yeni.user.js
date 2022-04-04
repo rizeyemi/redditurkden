67a4515f08b16ce09914f77388a75f6c8be41416
rizeyemi <102924655+rizeyemi@users.noreply.github.com>
2022-04-04 03:27:22 +0300

    Add files via upload

---
+++ yeni.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Reddit Place - Armée de Kameto
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  On va récuperer ce qui nous est du de droit.
+// @author       Adcoss95
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/rizeyemi/redditurkden/blob/main/photopng.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+ 
+    }, false);
+ 
+}
